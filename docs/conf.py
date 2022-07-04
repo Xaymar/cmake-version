8061b756976fca76e831c5f29e8f8b27d6f07b8e
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2022-07-04 22:47:51 +0200

    Version 1.1

--- docs/conf.py
+++ docs/conf.py
@@ -20,8 +20,8 @@
 project = 'version'
 copyright = "2022, Michael Fabian Dirks"
 author = "Michael Fabian 'Xaymar' Dirks"
-version = "1.0"
-release = "1.0"
+version = "1.1"
+release = "1.1"
 
 
 # -- General configuration ---------------------------------------------------
